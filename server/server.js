d3b3755c20188371547a7caf186a326aadc9cfd6
ee92 <eegor650@gmail.com>
2017-09-07 14:22:47 -0700

    socket.io connect/disconnect

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
-const express = require('express')
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-var app = express();
 const pubpath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(pubpath));
 
-app.listen(port, () => {
+io.on('connect', (socket) => {
+  console.log('new user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  });
+});
+
+server.listen(port, () => {
   console.log('server up on port ' + port);
 });
